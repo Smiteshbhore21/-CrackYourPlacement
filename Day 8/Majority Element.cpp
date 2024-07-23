2e6fea9b09baa535f8f6faa43d91d131f99f8e2e
Smiteshbhore21 <136716222+Smiteshbhore21@users.noreply.github.com>
2024-07-24 00:06:07 +0530

    Update Majority Element.cpp

--- Day 8/Majority Element.cpp
+++ Day 8/Majority Element.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for (int i=0; i<nums.size(); i++){
-            mp[nums[i]]++;
+        // unordered_map<int, int> mp;
+        // for (int i=0; i<nums.size(); i++){
+        //     mp[nums[i]]++;
+        // }
+        // for (auto it : mp)
+        // {
+        //     if (it.second > nums.size()/2) return it.first;
+        // }
+        // return -1;
+        int elem = nums[0], count = 0, cnt=0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count == 0) {
+                elem = nums[i];
+                count = 1;
+            } else if (elem == nums[i])
+                count++;
+            else
+                count--;
         }
-        for (auto it : mp)
-        {
-            if (it.second > nums.size()/2) return it.first;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == elem)
+                cnt++;
         }
+        if (cnt > nums.size() / 2)
+            return elem;
         return -1;
     }
 };
