9960713e370346f77116fc8ee6816cb36155233b
Smiteshbhore21 <136716222+Smiteshbhore21@users.noreply.github.com>
2024-07-31 20:49:45 +0530

    Update rabin_karp_algorithm_for_pattern_searching.cpp

--- Day 16/rabin_karp_algorithm_for_pattern_searching.cpp
+++ Day 16/rabin_karp_algorithm_for_pattern_searching.cpp
@@ -9,25 +9,21 @@ public:
         vector<int> res;
         int M = txt.length();
         int N = pat.length();
-        int p = 0; // hash value for pattern
-        int t = 0; // hash value for txt
+        int p = 0; 
+        int t = 0; 
         int h = 1;
-        int d = 256; // number of characters in the input alphabet
-        int q = 101; // a prime number
+        int d = 256;
+        int q = 101; 
 
-        // The value of h would be "pow(d, M-1)%q"
         for (int i = 0; i < M-1; i++)
             h = (h*d) % q;
 
-        // Calculate the hash value of pattern and first window of text
         for (int i = 0; i < M; i++) {
             p = (d*p + txt[i]) % q;
             t = (d*t + pat[i]) % q;
         }
 
-        // Slide the pattern over text one by one
         for (int i = 0; i <= N - M; i++) {
-            // Check the hash values of current window of text and pattern
             if ( p == t ) {
                 bool flag = true;
                 for (int j = 0; j < M; j++) {
@@ -40,7 +36,6 @@ public:
                     res.push_back(i);
             }
 
-            // Calculate hash value for next window of text: Remove leading digit, add trailing digit
             if ( i < N - M ) {
                 t = (d*(t - txt[0]*h) + pat[i + M]) % q;
                 if (t < 0)
